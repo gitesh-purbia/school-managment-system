7b697e92d2db3c6763a15b68dc5b8705f7b83747
devil23 <kunaldevil21@gmail.com>
2014-10-15 21:20:40 +0200

    info-file
    Signed-off-by: devil23 <kunaldevil21@gmail.com>

---
+++ add_info.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+    <body bgcolor="darkseagreen">
+        <form action="" method="POST" name="student">
+            <div align="center" style="vertical-align:top" widht="800px"> <table border="1"> <tr><td align="left">  Name:<input type="text" name="name"></td></tr><br><br>
+     <tr><td align="left">Father Name:<input type="text" name="fname"></td></tr><br><br>
+     <tr><td align="left">Mother Name:<input type="text" name="mname"></td></tr><br><br>
+     <tr><td align="left">Date of Birth:<input type="date" name="dob"></td></tr><br><br>
+     <tr><td align="left">Last School:<input type="text" name="lschool"></td></tr><br><br>
+     <tr><td align="left">Passed Class:<input type="text" name="pclass"></td></tr><br><br>
+     <tr><td align="left">Percentage:<input type="text" name="per"></td></tr><br><br>
+     <tr><td align="left">Phone Number:<input type="text" name="pnumber"></td></tr><br><br>
+     <tr><td align="left">Mobile Number:<input type="text" name="mnumber"></td></tr><br><br>
+     <tr><td align="left">Address:<input type="text" name="address"></td></tr><br><br>   
+     <tr><td align="left">Pin Code:<input type="text" name="pincode"></td></tr><br><br>    
+     <tr><td align="center"><input type="submit" value="ADD_INFO"</td></tr>
+                </div></table>
+        </form>
+       
+ 
+    </body>
+</html>
