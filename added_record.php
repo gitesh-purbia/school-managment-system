ec91d3b1b899ae0c51357be71bc250148a5d6959
devil23 <kunaldevil21@gmail.com>
2014-10-17 21:11:06 +0200

    edit code

--- added_record.php
+++ added_record.php
@@ -1,6 +1,10 @@
-<?php
+<html>
+    <head></head>
+    <title>School-Managment-system</title>
+    <body bgcolor="red">
+        
+    <?php
 include 'config.php';
-
 $name=$_POST['name'];
 $fname=$_POST['fname'];
 $mname=$_POST['mname'];
@@ -14,22 +18,37 @@ $mnumber=$_POST['mnumber'];
 $address=$_POST['address'];
 $pin=$_POST['pincode'];
 
-if (!isset($_post['submit']))
-{
+echo $name;
+echo $fname;
+echo $mname;
+echo $dob;
+echo $lschool;
+echo $pclass;
+echo $per;
+echo $pnumber;
+echo $mnumber;
+echo $address;
+echo $pin;
+
+if(mysqli_connect_error())
     
-    if(mysqli_connect_error())
-    {
-        echo 'Failed to connect to database' . mysqli_connect_error();
-    }
-    else
-    {
-    $sql= "INSERT INTO student" ."(name, fname, mname, dob, lschool, pclass , percentage, phonenumber, mobilenumber, address, pincode )". "VALUES ('$name','$fname', '$mname','$dob','$lschool','$pclass','$per','$pnumber','$mnumber','$address','$pin' )";
-    if(!mysqli_query($con, $sql))
-    {
-        die('error.'.mysqli_error($con));
-    }
-    echo 'record has been added';
+{
+echo 'failed to connect with database' . mysqli_connect_error();
+
 }
+$sql="INSERT INTO student"."(name,fname,mname,dob,lschool,pclass,percetange,pnumber,mnumber,address,pincode)"."VALUES('$name','$fname', '$mname', '$dob','$lschool','$pclass','$per','$pnumber','$mnumber','$address','$pin')";
+if(!mysqli_query($con, $sql)) 
+{
+    die('error:'.mysqli_error($con));
+    
+}
+
+else 
+{
+    echo '1 Record has been added';
+    
+    
 
 }
 ?>
+</body></html>
